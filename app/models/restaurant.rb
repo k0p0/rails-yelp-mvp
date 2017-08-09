aaf4c074c1a393b026141175bcd79bfd5ec0f7f9
Olivier Bellier <olivier@observices.fr>
2017-08-09 17:25:38 +0200

    increase validates

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  validates :name, uniqueness: true, presence: true
-  validates :address, presence: true
+  validates :name, uniqueness: true, presence: true, allow_blank: false
+  validates :address, presence: true, allow_blank: false
   validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :content, presence: true
+  validates :content, presence: true , allow_blank: false
   validates :rating, inclusion: { in: (0..5), allow_nil: false }, numericality: { only_integer: true }
 end
